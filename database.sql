44d0df7e36a893cce0526e3d008792a8482c43f3
Jovany Candelario <jovany.candel@gmail.com>
2021-01-07 19:32:55 -0800

    making changes

--- database.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE todo_database
-
-CREATE TABLE todo(
-    todo_id SERIAL PRIMIARY KEY,
-    description VARCHAR(255)
-)
--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require("express")
 const app = express()
 const pool = require("./db")
 
+const Pool = require("pg").Pool 
+
+const pool =  new Pool({
+    user:"postgres",
+    password:"T0nielt1gre",
+    database:"climate_change",
+    host: "localhost",
+    port: 5432
+})
+
+module.exports = pool;
+
 app.use(express.json())
 
 
